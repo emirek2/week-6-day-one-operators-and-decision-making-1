b609a7236fff247def5031db8207ab2fc8c875ee
Ezra Mirek <113930317+emirek2@users.noreply.github.com>
2022-09-28 17:41:39 +0000

    logical practice 3

--- main.py
+++ main.py
@@ -114,24 +114,27 @@ print(my_bool)
 # Create three variables (num1, num2, and num3):
 
 # Inside num1, store the value 36
-
+num1=36
 # Inside num2, stores the result of the operation 72/2
-
+num2=72/2
 # Inside num3, store the value 48
-
+num3=48
 # Check if num1 is greater than num2, or less than num3. Store the result of that comparison in a variable called my_bool.
-
+my_bool=(num1>num2)or(num1<num3)
+print(my_bool)
 
 
 # Logical Operators Practice #3
 # Check if the words:
 
 # word1 = "success", and
-
+word1 = "success"
 # word2 = "technology"
-
+word2 = "technology"
 # are not found in the sentence below, and store the result (a boolean) in a variable called my_bool:
-
+sentence='When something is important enough, you do it even if the odds are against you'
+my_bool=(word1 not in sentence)or(word2 not in sentence)
+print(my_bool)
 # "When something is important enough, you do it even if the odds are against you" - Elon Musk
 
 
